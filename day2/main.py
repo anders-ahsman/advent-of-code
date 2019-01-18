3ef96714a5393175478baf364cfcc21a8d3075ff
Anders Åhsman <emmry0@gmail.com>
2019-01-18 18:12:48 +0100

    Cleaner solution for day 2 part 2.

--- day2/main.py
+++ day2/main.py
@@ -19,9 +19,8 @@ print('Checksum:', checksum)
 # Part 2
 for idx, line in enumerate(data):
     for line2 in data[idx + 1:]:
-        differences = [abs(ord(x) - ord(y)) for x, y in zip(line, line2)]
-        only_one_different = differences.count(0) == len(differences) - 1
-        if only_one_different:
+        different = [x for x, y in zip(line, line2) if x != y]
+        if len(different) == 1:
             common = ''.join([x for x, y in zip(line, line2) if x == y])
             print('Common characters:', common)
             break
