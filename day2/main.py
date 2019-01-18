17808fab23a8d114b7b3eae31323ce7888ec07ac
Anders Åhsman <emmry0@gmail.com>
2019-01-18 18:00:50 +0100

    Day 2 part 2

--- day2/main.py
+++ day2/main.py
@@ -1,5 +1,3 @@
-import itertools
-
 with open('input.txt', 'r') as f:
     data = [x.strip() for x in f.readlines()]
 
@@ -17,3 +15,13 @@ for line in data:
             break
 checksum = twos * threes
 print('Checksum:', checksum)
+
+# Part 2
+for idx, line in enumerate(data):
+    for line2 in data[idx + 1:]:
+        differences = [abs(ord(x) - ord(y)) for x, y in zip(line, line2)]
+        only_one_different = differences.count(0) == len(differences) - 1
+        if only_one_different:
+            common = ''.join([x for x, y in zip(line, line2) if x == y])
+            print('Common characters:', common)
+            break
