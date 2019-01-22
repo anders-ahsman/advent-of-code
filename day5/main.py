66a97695a9724c689406c4364af44b3b9667bb7c
Anders Åhsman <emmry0@gmail.com>
2019-01-22 22:14:44 +0100

    Day 5 part 2

--- day5/main.py
+++ day5/main.py
@@ -1,3 +1,6 @@
+import re
+import string
+
 def read_polymer():
     with open('input.txt', 'r') as f:
         polymer = f.read()
@@ -18,7 +21,20 @@ def calc_final_length(polymer):
 def part1():
     polymer = read_polymer()
     final_length = calc_final_length(polymer)
-    print('Final length:', final_length)
+    print('Final length (part 1):', final_length)
+
+def part2():
+    polymer = read_polymer()
+    min_length = None
+
+    for c in string.ascii_lowercase:
+        after_remove = re.sub(c, '', polymer, flags=re.IGNORECASE)
+        length = calc_final_length(after_remove)
+        if not min_length or length < min_length:
+            min_length = length
+
+    print('Shortest final length (part 2):', min_length)
 
 if __name__ == '__main__':
     part1()
+    part2()
