f838fb23c7825ff41928b3fb2f05c05b3674985d
Anders Åhsman <anders.ahsman@designevolution.se>
2019-01-28 08:51:28 +0100

    Day 6 part 2 correct.

--- day6/main.py
+++ day6/main.py
@@ -6,7 +6,7 @@ def part1():
     closest_coord_map = calc_closest_coord_map(coordinates)
     closest_coord_map = remove_infinite_areas(closest_coord_map)
     max_area = calc_max_area(closest_coord_map)
-    print('Largest finite area:', max_area)
+    print('Part 1 | Largest finite area:', max_area)
 
 def read_coordinates():
     with open('input.txt', 'r') as f:
@@ -60,5 +60,32 @@ def calc_max_area(closest_coord_map):
     occurrences = most_common_closest_coord[1]
     return occurrences
 
+def part2():
+    coordinates = read_coordinates()
+    max_dist_sum = 10000
+    dist_sum_map = calc_distance_sum_map(coordinates, max_dist_sum)
+    area = calc_area(dist_sum_map)
+    print('Part 2 | Area:', area)
+
+def calc_distance_sum_map(coordinates, max_dist_sum):
+    # Manhattan distance from point to coordinate, total sum for all coordinates.
+    max_x = max(coordinates, key=lambda t: t[0])[0] + 1
+    max_y = max(coordinates, key=lambda t: t[1])[1] + 1
+    dist_sum_map = [[None for x in range(max_x)] for y in range(max_y)]
+    for y in range(max_y):
+        for x in range(max_x):
+            dist_sum = sum([manhattan_distance((x, y), c) for c in coordinates])
+            if dist_sum < max_dist_sum:
+                dist_sum_map[y][x] = dist_sum
+            else:
+                dist_sum_map[y][x] = None
+    return dist_sum_map
+
+def calc_area(dist_sum_map):
+    not_none = [dist for dist in list(it.chain.from_iterable(dist_sum_map)) if dist != None]
+    area = len(not_none)
+    return area
+
 if __name__ == '__main__':
     part1()
+    part2()
