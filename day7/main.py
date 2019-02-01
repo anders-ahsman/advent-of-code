656eb54f56075b831871494e025f19b19f1c81a3
Anders Åhsman <anders.ahsman@designevolution.se>
2019-02-01 17:15:39 +0100

    Working day 7 part 1.

--- day7/main.py
+++ day7/main.py
@@ -1,16 +1,52 @@
 import re
 
-def read_steps():
-    steps = []
+def part1():
+    requirements = read_requirements()
+    order = determine_order(requirements)
+    print('Part 1:', order)
+
+def read_requirements():
+    requirements = []
     with open('input.txt', 'r') as f:
         for row in f.readlines():
             m = re.match(r'Step (\w) must be finished before step (\w) can begin.', row)
-            steps.append((m.group(1), m.group(2)))
-    return tuple(steps)
+            requirements.append((m.group(1), m.group(2)))
+    return tuple(requirements)
 
-def part1():
-    steps = read_steps()
-    print(steps)
+def determine_order(requirements):
+    order = ''
+
+    unique_steps = get_unique_steps(requirements)
+    unique_steps_initial_count = len(unique_steps)
+    while len(order) < unique_steps_initial_count:
+        steps_only_in_first = []
+        for step in unique_steps:
+            only_in_first = True
+            for requirement in requirements:
+                second = requirement[1]
+                if second == step:
+                    only_in_first = False
+                    break
+
+            if only_in_first:
+                steps_only_in_first.append(step)
+
+        steps_only_in_first.sort()
+        step_to_add = steps_only_in_first[0]
+
+        requirements = [r for r in requirements if r[0] != step_to_add]
+        unique_steps = [s for s in unique_steps if s != step_to_add]
+
+        order += step_to_add
+
+    return order
+
+def get_unique_steps(requirements):
+    unique = set()
+    for s in requirements:
+        unique.add(s[0])
+        unique.add(s[1])
+    return unique
 
 if __name__ == '__main__':
     part1()
