6462ce3b6acb23fe6fb471a3375f329461bb501f
Anders Åhsman <emmry0@gmail.com>
2019-02-02 11:41:16 +0100

    Tests for day 2.

--- day2/main.py
+++ day2/main.py
@@ -1,25 +1,38 @@
-with open('input.txt', 'r') as f:
-    data = [x.strip() for x in f.readlines()]
+def part1():
+    data = read_input()
 
-# Part 1
-twos = 0
-threes = 0
-for line in data:
-    for char in line:
-        if line.count(char) == 2:
-            twos += 1
-            break
-    for char in line:
-        if line.count(char) == 3:
-            threes += 1
-            break
-checksum = twos * threes
-print('Checksum:', checksum)
+    twos = 0
+    threes = 0
+    for line in data:
+        for char in line:
+            if line.count(char) == 2:
+                twos += 1
+                break
+        for char in line:
+            if line.count(char) == 3:
+                threes += 1
+                break
+    checksum = twos * threes
+    return checksum
 
-# Part 2
-from itertools import combinations
-for line1, line2 in combinations(data, 2):
-    different = [x for x, y in zip(line1, line2) if x != y]
-    if len(different) == 1:
-        common = ''.join([x for x, y in zip(line1, line2) if x == y])
-        print('Common characters:', common)
+def read_input():
+    with open('input.txt', 'r') as f:
+        data = [x.strip() for x in f.readlines()]
+    return data
+
+def part2():
+    data = read_input()
+
+    from itertools import combinations
+    for line1, line2 in combinations(data, 2):
+        different = [x for x, y in zip(line1, line2) if x != y]
+        if len(different) == 1:
+            common = ''.join([x for x, y in zip(line1, line2) if x == y])
+            return common
+
+if __name__ == '__main__':
+    checksum = part1()
+    print('Checksum:', checksum)
+
+    common_chars = part2()
+    print('Common characters:', common_chars)
