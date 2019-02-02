d49ceb0478a36f39b1639646c0752f7ce47efe84
Anders Åhsman <emmry0@gmail.com>
2019-02-02 11:31:20 +0100

    Tests for day 1.

--- day1/main.py
+++ day1/main.py
@@ -1,17 +1,27 @@
-with open('input.txt', 'r') as f:
-    data = [int(x) for x in f.readlines()]
+def part1():
+    data = read_input()
+    freq_sum = sum(data)
+    return freq_sum
 
-# Part 1
-freq_sum = sum(data)
-print('Sum of frequencies:', freq_sum)
+def read_input():
+    with open('input.txt', 'r') as f:
+        data = [int(x) for x in f.readlines()]
+    return data
 
-# Part 2
-seen = {0}
-freq = 0
-from itertools import cycle
-for num in cycle(data):
-    freq += num
-    if freq in seen:
-        print('First repeating frequency:', freq)
-        break
-    seen.add(freq)
+def part2():
+    data = read_input()
+    seen = {0}
+    freq = 0
+    from itertools import cycle
+    for num in cycle(data):
+        freq += num
+        if freq in seen:
+            return freq
+        seen.add(freq)
+
+if __name__ == '__main__':
+    freq_sum = part1()
+    print('Frequence sum:', freq_sum)
+
+    first_repeating_freq = part2()
+    print('First repeating frequency:', first_repeating_freq)
