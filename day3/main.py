0ea073caf012a5e6c45bc36752dd1c64683f9b2b
Anders Åhsman <emmry0@gmail.com>
2019-02-02 12:00:25 +0100

    Day 3 added tests and refactored.

--- day3/main.py
+++ day3/main.py
@@ -1,14 +1,18 @@
 import numpy as np
 import re
 
-with open('input.txt', 'r') as f:
-    data = [x.strip() for x in f.readlines()]
+def part1():
+    claims = load_claims()
+    overlap_count = calc_overlap_count(claims)
+    print('Claimed more than once:', overlap_count)
+    return overlap_count
 
 class Claim(object):
     pass
 
 def load_claims():
     claims = []
+    data = read_input()
     for row in data:
         m = re.match(r'^#(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)$', row)
         c = Claim()
@@ -20,23 +24,41 @@ def load_claims():
         claims.append(c)
     return claims
 
-def calc_overlaps(claims):
+def read_input():
+    with open('input.txt', 'r') as f:
+        data = [x.strip() for x in f.readlines()]
+    return data
+
+def calc_overlap_count(claims):
     size = 1000
     fabric = np.zeros((size, size))
 
     for c in claims:
         fabric[c.x:c.x + c.width, c.y:c.y + c.height] += 1
 
-    claimed_more_than_once = 0
+    overlap_count = 0
     for cell in np.nditer(fabric):
         if cell > 1:
-            claimed_more_than_once += 1
-    print('Claimed more than once:', claimed_more_than_once)
+            overlap_count += 1
+    return overlap_count
+
+def part2():
+    claims = load_claims()
+    claim_id_intact = calc_claim_id_intact(claims)
+    print('Claims intact for ID:', claim_id_intact)
+    return claim_id_intact
+
+def calc_claim_id_intact(claims):
+    size = 1000
+    fabric = np.zeros((size, size))
+
+    for c in claims:
+        fabric[c.x:c.x + c.width, c.y:c.y + c.height] += 1
 
     for c in claims:
         if all(cell == 1 for cell in np.nditer(fabric[c.x:c.x + c.width, c.y:c.y + c.height])):
-            print('Claims intact for ID:', c.id)
-            break
+            return c.id
 
-claims = load_claims()
-calc_overlaps(claims)
+if __name__ == '__main__':
+    part1()
+    part2()
