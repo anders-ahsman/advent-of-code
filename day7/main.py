1dbef40509d1a7fab35ef30d3d9df7a6b8669ad9
Anders Åhsman <emmry0@gmail.com>
2019-02-16 10:24:53 +0100

    Day 7 part 2

--- day7/main.py
+++ day7/main.py
@@ -6,6 +6,13 @@ def part1():
     print('Part 1:', order)
     return order
 
+def part2():
+    requirements = read_requirements()
+    worker_count = 5
+    time = calc_total_time(requirements, worker_count)
+    print('Part 2:', time)
+    return time
+
 def read_requirements():
     requirements = []
     with open('input.txt', 'r') as f:
@@ -15,22 +22,17 @@ def read_requirements():
     return requirements
 
 def calc_step_order(requirements):
-    step_order = ''
+    order = ''
 
     steps = get_unique_steps(requirements)
     while len(steps) > 0:
-        steps_only_left_side = []
-        for step in steps:
-            if not [r for r in requirements if r[1] == step]:
-                steps_only_left_side.append(step)
-
-        steps_only_left_side.sort()
-        step_added = steps_only_left_side[0]
-        step_order += step_added
+        steps_available = get_steps_only_left_side(steps, requirements)
+        step_added = steps_available[0]
+        order += step_added
         steps.remove(step_added)
         requirements = [r for r in requirements if r[0] != step_added]
 
-    return step_order
+    return order
 
 def get_unique_steps(requirements):
     unique_steps = set()
@@ -39,5 +41,47 @@ def get_unique_steps(requirements):
         unique_steps.add(r[1])
     return unique_steps
 
+def get_steps_only_left_side(steps, requirements):
+    only_left = []
+    for step in steps:
+        if not [r for r in requirements if r[1] == step]:
+            only_left.append(step)
+    only_left.sort()
+    return only_left
+
+def calc_total_time(requirements, worker_count):
+    time = -1
+    workers = []
+    for i in range(worker_count):
+        workers.append([0, None])
+
+    steps = get_unique_steps(requirements)
+    while len(steps) > 0:
+        time += 1
+
+        # Has any step completed?
+        for idx, worker in enumerate(workers):
+            if worker[0] == time and worker[1] != None:
+                step_added = worker[1]
+                steps.remove(step_added)
+                requirements = [r for r in requirements if r[0] != step_added]
+
+        steps_available = get_steps_only_left_side(steps, requirements)
+        for step in steps_available:
+            step_in_progress = len([w for w in workers if w[1] == step]) > 0
+            if step_in_progress:
+                continue
+
+            for idx, worker in enumerate(workers):
+                worker_available = worker[0] <= time
+                if worker_available:
+                    worker[0] = time + ord(step) - 4
+                    worker[1] = step
+                    workers[idx] = worker
+                    break
+
+    return time
+
 if __name__ == '__main__':
     part1()
+    part2()
--- day7/tests.py
+++ day7/tests.py
@@ -1,10 +1,13 @@
 import unittest
 
-from main import part1
+from main import part1, part2
 
 class Tests(unittest.TestCase):
     def test_part1_returns_expected_answer(self):
         self.assertEqual(part1(), 'AHJDBEMNFQUPVXGCTYLWZKSROI')
+
+    def test_part2_returns_expected_answer(self):
+        self.assertEqual(part2(), 1031)
 
 if __name__ == '__main__':
     unittest.main()
