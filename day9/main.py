00fb8214065dc2de3e2b362904f0364dd18e1476
Anders Åhsman <anders.ahsman@designevolution.se>
2019-02-19 09:29:07 +0100

    Inspired by other solutions to use deque instead of custom DoublyLinkedList class. Now tests run in about 3s!

--- day9/main.py
+++ day9/main.py
@@ -1,5 +1,5 @@
 import re
-from collections import defaultdict
+from collections import defaultdict, deque
 
 def part1():
     players, last_marble = read_input()
@@ -22,48 +22,19 @@ def read_input():
         m = re.match(r'(\d+) players; last marble is worth (\d+) points', row)
         return int(m.group(1)), int(m.group(2))
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-        self.prev = None
-
-class DoublyLinkedList:
-    def __init__(self, node):
-        node.next = node
-        node.prev = node
-        self.cur_node = node
-
-    def add_node(self, new_node):
-        # Insert two positions right of current and move current to inserted
-        new_node.next = self.cur_node.next.next
-        new_node.prev = self.cur_node.next
-        self.cur_node.next.next.prev = new_node
-        self.cur_node.next.next = new_node
-        self.cur_node = new_node
-
-    def remove_at_pos(self, n):
-        clockwise = n > 0
-        for _ in range(abs(n)):
-            self.cur_node = self.cur_node.next if clockwise else self.cur_node.prev
-
-        removed_value = self.cur_node.value
-
-        self.cur_node.prev.next = self.cur_node.next
-        self.cur_node.next.prev = self.cur_node.prev
-        self.cur_node = self.cur_node.next
-
-        return removed_value
-
 def play_marble(player_count, last_marble):
     score = defaultdict(int)
-    circle = DoublyLinkedList(Node(0))
+    circle = deque([0])
 
+    # Current position at right end of deque
     for marble in range(1, last_marble + 1):
         if marble % 23 == 0:
-            score[marble % player_count] += marble + circle.remove_at_pos(-7)
+            circle.rotate(7)
+            score[marble % player_count] += marble + circle.pop()
+            circle.rotate(-1)
         else:
-            circle.add_node(Node(marble))
+            circle.rotate(-1)
+            circle.append(marble)
 
     high_score = max(score.values())
     return high_score
