fb0d54acffc59597fba92759ea7a4a651d87a295
Anders Åhsman <anders.ahsman@gmail.com>
2019-10-19 23:27:42 +0200

    Day 2 part 2.

--- 2017/day2/main.py
+++ 2017/day2/main.py
@@ -1,13 +1,20 @@
 import re
 
-def part1():
+def part1(rows):
     checksum = 0
-    rows = read_input()
     for row in rows:
         diff = max(row) - min(row)
         checksum += diff
+    return checksum
 
-    print(checksum)
+def part2(rows):
+    checksum = 0
+    for row in rows:
+        for this in row:
+            for other in row:
+                if this != other and other % this == 0:
+                    checksum += int(other / this)
+    return checksum
 
 def read_input():
     with open('input.txt', 'r') as f:
@@ -17,4 +24,8 @@ def read_input():
         return rows
 
 if __name__ == '__main__':
-    part1()
+    rows = read_input()
+    checksum1 = part1(rows)
+    checksum2 = part2(rows)
+
+    print(checksum1, checksum2)
