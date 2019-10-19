4e9aabce94be4370bbb622fed48b527033b38873
Anders Åhsman <anders.ahsman@gmail.com>
2019-10-19 20:38:50 +0200

    Working part 2.

--- 2017/day1/main.py
+++ 2017/day1/main.py
@@ -1,18 +1,12 @@
-def part1():
+def part2():
     digits = [int(c) for c in read_input()]
-    digits.append(digits[0])
-
     match_sum = 0
-    last_digit = None
-    for digit in digits:
-        if last_digit == None:
-            last_digit = digit
-            continue
 
-        if digit == last_digit:
+    for i, digit in enumerate(digits):
+        pos = i + int(len(digits) / 2 - len(digits))
+        match = digit == digits[pos]
+        if match:
             match_sum += digit
-
-        last_digit = digit
 
     print(match_sum)
 
@@ -22,4 +16,4 @@ def read_input():
         return s
 
 if __name__ == '__main__':
-    part1()
+    part2()
