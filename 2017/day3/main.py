0dea84579b5d2bf87295b462fa836855d3841b81
Anders Åhsman <anders.ahsman@gmail.com>
2019-10-26 17:41:31 +0200

    Extracted function to set value.

--- 2017/day3/main.py
+++ 2017/day3/main.py
@@ -1,7 +1,7 @@
 from itertools import cycle
 from math import floor, sqrt
 
-def part1(target_value):
+def part1(target_value, calc_value):
     directions = cycle([(1, 0), (0, -1), (-1, 0), (0, 1)])
     move_dir = next(directions)
 
@@ -12,19 +12,19 @@ def part1(target_value):
 
     start_pos = (floor(size / 2), floor(size / 2))
     pos = start_pos
-    value = set_at_pos_and_increase_value(a, pos, 1)
+    value = calc_value(a, pos)
 
     while value <= target_value:
         # Set current
         pos = add_vecs(pos, move_dir)
-        value = set_at_pos_and_increase_value(a, pos, value)
+        value = calc_value(a, pos)
 
         # Keep moving until able to turn
         check_dir = next(directions)
         check_pos = add_vecs(pos, check_dir)
         while value <= target_value and a[check_pos[1]][check_pos[0]] != 0:
             pos = add_vecs(pos, move_dir)
-            value = set_at_pos_and_increase_value(a, pos, value)
+            value = calc_value(a, pos)
             check_pos = add_vecs(pos, check_dir)
 
         move_dir = check_dir
@@ -35,10 +35,20 @@ def part1(target_value):
 def add_vecs(a, b):
     return tuple([sum(x) for x in zip(a, b)])
 
-def set_at_pos_and_increase_value(a, pos, value):
-    a[pos[1]][pos[0]] = value
-    return value + 1
+class Object(object):
+    pass
+
+def part1_set_value():
+    scope = Object()
+    scope.count = 1
+
+    def inner(a, pos):
+        a[pos[1]][pos[0]] = scope.count
+        scope.count += 1
+        return scope.count
+
+    return inner
 
 if __name__ == '__main__':
-    steps = part1(368078)
+    steps = part1(368078, part1_set_value())
     print(steps)
