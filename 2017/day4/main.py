1a6c3f5d5667773ca89fc3e398397ea0b701f310
Anders Åhsman <anders.ahsman@gmail.com>
2019-10-27 11:06:24 +0100

    Completed part 2.

--- 2017/day4/main.py
+++ 2017/day4/main.py
@@ -3,7 +3,9 @@ def read_input():
         return [row.rstrip().split(' ') for row in f.readlines()]
 
 def part1(passphrases):
-    return sum(1 for passphrase in passphrases if not has_duplicate_words(passphrase))
+    valid_count = sum(1 for passphrase in passphrases\
+        if not has_duplicate_words(passphrase))
+    return valid_count
 
 def has_duplicate_words(passphrase):
     s = set()
@@ -13,6 +15,28 @@ def has_duplicate_words(passphrase):
         s.add(word)
     return False
 
+def part2(passphrases):
+    valid_count = sum(1 for passphrase in passphrases \
+        if not has_duplicate_words_as_anagrams(passphrase))
+    return valid_count
+
+def has_duplicate_words_as_anagrams(passphrase):
+    s = set()
+    for word in passphrase:
+        if any(w for w in s if are_anagrams(w, word)):
+            return True
+        s.add(word)
+    return False
+
+def are_anagrams(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    for c1 in s1:
+        if sum(1 for c in s1 if c == c1) != sum(1 for c in s2 if c == c1):
+            return False
+    return True
+
 if __name__ == '__main__':
     passphrases = read_input()
     print(part1(passphrases))
+    print(part2(passphrases))
