9d9257c4f5c2ff9c79f3f89bd4816d7149017d53
Anders Åhsman <anders.ahsman@gmail.com>
2019-12-13 10:50:39 +0100

    Refactored to use complete IntcodeComputer from shared module.

--- 2019/day02/main.py
+++ 2019/day02/main.py
@@ -1,35 +1,42 @@
 import sys
 
-def read_input():
-    opcodes = [int(x) for x in next(sys.stdin).split(',')]
-    return opcodes
+sys.path.append('..')
+from common.intcode_computer import IntcodeComputer
 
-def main(opcodes):
-    opcodes_copy = opcodes[:]
-    pos = 0
+def read_input():
+    program = [int(x) for x in next(sys.stdin).split(',')]
+    return program
+
+def part1(program):
+    computer = IntcodeComputer(program)
+    computer.program[1] = 12
+    computer.program[2] = 2
+    it = computer.run()
+    try:
+        while True:
+            next(it)
+    except StopIteration:
+        value = computer.program[0]
+        print(f'part1: {value}')
+
+def part2(program):
+    expected = 19690720
     for noun in range(100):
         for verb in range(100):
-            opcodes[1] = noun
-            opcodes[2] = verb
+            computer = IntcodeComputer(program)
+            computer.program[1] = noun
+            computer.program[2] = verb
+            it = computer.run()
 
-            while True:
-                op = opcodes[pos]
-                if op == 1:
-                    opcodes[opcodes[pos + 3]] = opcodes[opcodes[pos + 1]] + opcodes[opcodes[pos + 2]]
-                elif op == 2:
-                    opcodes[opcodes[pos + 3]] = opcodes[opcodes[pos + 1]] * opcodes[opcodes[pos + 2]]
-                elif op == 99:
-                    break
-
-                pos += 4
-
-            if opcodes[0] == 19690720:
-                return noun, verb
-            else:
-                opcodes = opcodes_copy[:]
-                pos = 0
+            try:
+                while True:
+                    next(it)
+            except StopIteration:
+                value = computer.program[0]
+                if value == expected:
+                    print(f'part2: {100 * noun + verb}')
 
 if __name__ == '__main__':
-    opcodes = read_input()
-    noun, verb = main(opcodes)
-    print('100 * noun + verb:', 100 * noun + verb)
+    program = read_input()
+    part1(program)
+    part2(program)
