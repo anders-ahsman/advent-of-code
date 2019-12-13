77215a2d416756a6e46bae912261e6f6771ce084
Anders Åhsman <anders.ahsman@gmail.com>
2019-12-13 14:08:12 +0100

    Added Tile enum.

--- 2019/day13/main.py
+++ 2019/day13/main.py
@@ -1,26 +1,35 @@
 import sys
+from enum import Enum
 
 sys.path.append('..')
 from common.intcode_computer import IntcodeComputer
+
+class Tile(Enum):
+    EMPTY = 0
+    WALL = 1
+    BLOCK = 2
+    PADDLE = 3
+    BALL = 4
 
 def read_input():
     program = [int(x) for x in next(sys.stdin).split(',')]
     return program
 
-def main(program):
+def part1(program):
     computer = IntcodeComputer(program)
     it = computer.run()
+
     try:
         block_count = 0
         while True:
-            x, y, output = next(it), next(it), next(it)
-            if output == 2:
+            x, y, tile = next(it), next(it), Tile(next(it))
+            if tile == Tile.BLOCK:
                 block_count += 1
     except StopIteration:
         pass
 
-    print(block_count)
+    print(f'part1: {block_count}')
 
 if __name__ == '__main__':
     program = read_input()
-    main(program)
+    part1(program)
