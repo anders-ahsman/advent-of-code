27343d2545d31ef381e420b004df144ca0793016
Anders Åhsman <anders.ahsman@gmail.com>
2019-12-13 10:56:45 +0100

    Refactored day 5 to use IntcodeComputer from shared module.

--- 2019/day05/main.py
+++ 2019/day05/main.py
@@ -1,142 +1,25 @@
-from enum import Enum
 import sys
 
-class Mode(Enum):
-    POSITION = 0
-    IMMEDIATE = 1
-
-class Instruction(Enum):
-    ADD = 1
-    MULTIPLY = 2
-    INPUT = 3
-    OUTPUT = 4
-    JUMP_IF_TRUE = 5
-    JUMP_IF_FALSE = 6
-    LESS_THAN = 7
-    EQUALS = 8
-    ABORT = 99
+sys.path.append('..')
+from common.intcode_computer import IntcodeComputer
 
 def read_input():
-    opcodes = [int(x) for x in next(sys.stdin).split(',')]
-    return opcodes
+    program = [int(x) for x in next(sys.stdin).split(',')]
+    return program
 
-def main(opcodes, indata):
-    pos = 0
-    outdata = None
+def main(program, indata):
+    computer = IntcodeComputer(program)
+    computer.inputs.append(indata)
+    it = computer.run()
 
-    while True:
-        opcode = opcodes[pos]
-        mode_1st_param, mode_2nd_param, mode_3rd_param = set_modes(opcode)
-
-        opcode = int(str(opcode)[-2:])
-        if opcode == Instruction.ADD.value:
-            val1 = get_param(opcodes, pos + 1, mode_1st_param)
-            val2 = get_param(opcodes, pos + 2, mode_2nd_param)
-            result = val1 + val2
-
-            if mode_3rd_param == Mode.POSITION:
-                opcodes[opcodes[pos + 3]] = result
-            else:
-                opcodes[pos + 3] = result
-
-            pos += 4
-
-        elif opcode == Instruction.MULTIPLY.value:
-            val1 = get_param(opcodes, pos + 1, mode_1st_param)
-            val2 = get_param(opcodes, pos + 2, mode_2nd_param)
-            result = val1 * val2
-
-            if mode_3rd_param == Mode.POSITION:
-                opcodes[opcodes[pos + 3]] = result
-            else:
-                opcodes[pos + 3] = result
-            pos += 4
-
-        elif opcode == Instruction.INPUT.value:
-            opcodes[opcodes[pos + 1]] = indata
-            pos += 2
-
-        elif opcode == Instruction.OUTPUT.value:
-            outdata = opcodes[opcodes[pos + 1]]
-            pos += 2
-
-        elif opcode == Instruction.JUMP_IF_TRUE.value:
-            val1 = get_param(opcodes, pos + 1, mode_1st_param)
-            val2 = get_param(opcodes, pos + 2, mode_2nd_param)
-            pos = val2 if val1 else pos + 3
-
-        elif opcode == Instruction.JUMP_IF_FALSE.value:
-            val1 = get_param(opcodes, pos + 1, mode_1st_param)
-            val2 = get_param(opcodes, pos + 2, mode_2nd_param)
-            pos = val2 if not val1 else pos + 3
-
-        elif opcode == Instruction.LESS_THAN.value:
-            val1 = get_param(opcodes, pos + 1, mode_1st_param)
-            val2 = get_param(opcodes, pos + 2, mode_2nd_param)
-            result = 1 if val1 < val2 else 0
-
-            if mode_3rd_param == Mode.POSITION:
-                opcodes[opcodes[pos + 3]] = result
-            else:
-                opcodes[pos + 3] = result
-
-            pos += 4
-
-        elif opcode == Instruction.EQUALS.value:
-            val1 = get_param(opcodes, pos + 1, mode_1st_param)
-            val2 = get_param(opcodes, pos + 2, mode_2nd_param)
-            result = 1 if val1 == val2 else 0
-
-            if mode_3rd_param == Mode.POSITION:
-                opcodes[opcodes[pos + 3]] = result
-            else:
-                opcodes[pos + 3] = result
-
-            pos += 4
-
-        elif opcode == Instruction.ABORT.value:
-            return outdata
-
-        else:
-            raise Exception(f'Unknown opcode {opcode}')
-
-def set_modes(opcode):
-    if len(str(opcode)) > 2:
-        modes = str(opcode)[:-2]
-
-        try:
-            mode_1st_param = Mode(int(modes[-1:]))
-        except ValueError:
-            mode_1st_param = Mode.POSITION
-        modes = modes[:-1]
-
-        try:
-            mode_2nd_param = Mode(int(modes[-1:]))
-        except ValueError:
-            mode_2nd_param = Mode.POSITION
-        modes = modes[:-1]
-
-        try:
-            mode_3rd_param = Mode(int(modes[-1:]))
-        except ValueError:
-            mode_3rd_param = Mode.POSITION
-    else:
-        mode_1st_param = Mode.POSITION
-        mode_2nd_param = Mode.POSITION
-        mode_3rd_param = Mode.POSITION
-
-    return (mode_1st_param, mode_2nd_param, mode_3rd_param)
-
-def get_param(opcodes, pos, mode):
-    if mode == Mode.POSITION:
-        return opcodes[opcodes[pos]]
-    elif mode == Mode.IMMEDIATE:
-        return opcodes[pos]
-
-    raise Exception(f'Unknown mode {mode}')
+    last_output = None
+    try:
+        while True:
+            last_output = next(it)
+    except StopIteration:
+        print(last_output)
 
 if __name__ == '__main__':
-    opcodes = read_input()
-
-    print(main(opcodes[:], 1))
-    print(main(opcodes[:], 5))
+    program = read_input()
+    main(program, 1)
+    main(program, 5)
