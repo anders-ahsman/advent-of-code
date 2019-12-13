52050d297f6aadf5df666500550bb1fb8a2e8ec2
Anders Åhsman <anders.ahsman@gmail.com>
2019-12-13 06:20:38 +0100

    Day 13 part 1.

---
+++ 2019/day13/main.py
@@ -0,0 +1,24 @@
+import sys
+from intcode_computer import IntcodeComputer
+
+def read_input():
+    program = [int(x) for x in next(sys.stdin).split(',')]
+    return program
+
+def main(program):
+    computer = IntcodeComputer(program)
+    it = computer.run()
+    try:
+        block_count = 0
+        while True:
+            x, y, output = next(it), next(it), next(it)
+            if output == 2:
+                block_count += 1
+    except StopIteration:
+        pass
+
+    print(block_count)
+
+if __name__ == '__main__':
+    program = read_input()
+    main(program)
