efed7746f18847d8fa27efdd4b52885b88eff893
Anders Åhsman <anders.ahsman@gmail.com>
2019-12-16 06:45:19 +0100

    Day 16 part 1 complete.

---
+++ 2019/day16/main.py
@@ -0,0 +1,43 @@
+import sys
+
+def read_input():
+    digits = [int(char) for char in next(sys.stdin)]
+    return digits
+
+def main(digits):
+    digits = calc_digits(digits, 100)
+    print(digits)
+
+def calc_digits(digits, count):
+    for _ in range(count):
+        digits_copy = digits[:]
+        for i in range(len(digits)):
+            it = get_pattern_iter(i + 1)
+            digit_sum = 0
+            for j in range(len(digits)):
+                digit_sum += digits_copy[j] * next(it)
+            digits[i] = int(str(digit_sum)[-1])
+    return digits
+
+def get_pattern_iter(repeat_target):
+    digits = (0, 1, 0, -1)
+    pos = 0
+    repeat_count = 0
+    digit = digits[pos]
+    is_first = True
+    while True:
+        if is_first:
+            is_first = False
+        else:
+            yield digit
+
+        repeat_count += 1
+        if repeat_count >= repeat_target:
+            repeat_count = 0
+            pos = (pos + 1) % len(digits)
+            digit = digits[pos]
+
+
+if __name__ == '__main__':
+    digits = read_input()
+    main(digits)
