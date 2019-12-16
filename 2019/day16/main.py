c6bcfb8da51147374aa31d6b0c1a7f814441f7fb
Anders Åhsman <anders.ahsman@gmail.com>
2019-12-16 16:32:32 +0100

    Part 1 optimizations.

--- 2019/day16/main.py
+++ 2019/day16/main.py
@@ -8,14 +8,12 @@ def main(digits):
     digits = calc_digits(digits, 100)
     print(f'Part 1: {digits}')
 
-def calc_digits(digits, count):
-    for _ in range(count):
-        digits_copy = digits[:]
-        for i in range(len(digits)):
+def calc_digits(digits, phase_count):
+    digits_range = range(len(digits))
+    for _ in range(phase_count):
+        for i in digits_range:
             it = get_pattern_iter(i + 1)
-            digit_sum = 0
-            for j in range(len(digits)):
-                digit_sum += digits_copy[j] * next(it)
+            digit_sum = sum([digits[j] * next(it) for j in digits_range])
             digits[i] = int(str(digit_sum)[-1])
     return ''.join([str(d) for d in digits[0:8]])
 
@@ -36,7 +34,6 @@ def get_pattern_iter(repeat_target):
             repeat_count = 0
             pos = (pos + 1) % len(digits)
             digit = digits[pos]
-
 
 if __name__ == '__main__':
     digits = read_input()
