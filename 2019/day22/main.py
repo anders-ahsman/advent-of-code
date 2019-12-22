cf5f9d12568e3eb804cb44230195475c9f35ab2e
Anders Åhsman <anders.ahsman@gmail.com>
2019-12-22 07:28:06 +0100

    Better variable names, small optimization.

--- 2019/day22/main.py
+++ 2019/day22/main.py
@@ -5,39 +5,38 @@ def read_input():
     return shuffles
 
 def part1(shuffles):
-    cards = list(range(10007))
+    deck = list(range(10007))
     for shuffle in shuffles:
         if 'deal with increment' in shuffle:
             increment = int(shuffle.split(' ')[-1])
-            cards = deal_with_increment(cards, increment)
+            deck = deal_with_increment(deck, increment)
         elif 'cut' in shuffle:
             count = int(shuffle.split(' ')[-1])
-            cards = cut(cards, count)
+            deck = cut(deck, count)
         elif 'deal into new stack' == shuffle:
-            cards = deal_into_new_stack(cards)
+            deck = deal_into_new_stack(deck)
         else:
             raise Exception(f'Unknown shuffle {shuffle}')
 
-    print('Part 1:', cards.index(2019))
+    print('Part 1:', deck.index(2019))
 
-def cut(cards, count):
-    shuffled = cards[count:] + cards[:count]
+def cut(deck, count):
+    shuffled = deck[count:] + deck[:count]
     return shuffled
 
-def deal_into_new_stack(cards):
-    shuffled = cards[:]
-    shuffled.reverse()
-    return shuffled
+def deal_into_new_stack(deck):
+    deck.reverse()
+    return deck
 
-def deal_with_increment(cards, increment):
-    card_count = len(cards)
-    shuffled = [None] * card_count
+def deal_with_increment(deck, increment):
+    deck_size = len(deck)
+    shuffled = [None] * deck_size
     pos = 0
-    while cards:
-        shuffled[pos] = cards[0]
-        cards = cards[1:]
+    while deck:
+        shuffled[pos] = deck[0]
+        deck = deck[1:]
         pos += increment
-        pos = pos % card_count
+        pos = pos % deck_size
     return shuffled
 
 if __name__ == '__main__':
