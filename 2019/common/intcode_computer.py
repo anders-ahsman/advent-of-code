d09914511330696abc9b1c4e3ceb740e1d28c5d5
Anders Åhsman <anders.ahsman@gmail.com>
2019-12-28 21:08:48 +0100

    Use pop(0) when reading input.

--- 2019/common/intcode_computer.py
+++ 2019/common/intcode_computer.py
@@ -46,8 +46,7 @@ class IntcodeComputer:
                 self.idx += 4
 
             elif instruction == Instruction.INPUT:
-                indata = self.inputs[0]
-                self.inputs = self.inputs[1:]
+                indata =  self.inputs.pop(0)
                 self.set_value(mode1, 1, indata)
                 self.idx += 2
 
