4c70ee7d51ef44db660b05889167d55d3544d13e
Anders Åhsman <anders.ahsman@gmail.com>
2019-12-29 21:07:55 +0100

    Part 1 WIP. Working BFS implementation but does not find total shortest path.

--- 2019/day18/main.py
+++ 2019/day18/main.py
@@ -1,4 +1,9 @@
-import sys
+from collections import deque
+import re, sys
+
+pattern_accessible = re.compile('[.@a-z]')
+pattern_door = re.compile('[A-Z]')
+pattern_key = re.compile('[a-z]')
 
 def read_input():
     maze = {}
@@ -9,19 +14,60 @@ def read_input():
     return maze
 
 def part1(maze):
-    pos = None
-    for y in range(30):
-        for x in range(100):
-            if (x, y) in maze:
-                output = maze[(x, y)]
-                if output == '@':
-                    output = '.'
-                    pos = (x, y)
-            else:
-                output = ' '
+    for y in range(max(pos[1] for pos in maze) + 1):
+        for x in range(max(pos[0] for pos in maze) + 1):
+            output = maze[(x, y)] if (x, y) in maze else ' '
             print(output, end='')
         print()
-    print(pos)
+
+    initial = next(pos for pos, value in maze.items() if value == '@')
+    keys_to_collect = {value for key, value in maze.items() if pattern_key.match(value)}
+    steps = min_steps_to_keys(maze, initial, keys_to_collect)
+    print(f'steps: {steps}')
+
+def min_steps_to_keys(maze, initial, keys_to_collect, keys=set()):
+    frontier = deque([initial])
+    explored = {initial}
+    nodes_to_parents = {}
+    while frontier:
+        node = frontier.popleft()
+        value = maze[node]
+        if pattern_key.match(value) and value not in keys:
+            keys.add(value)
+            depth = steps_to_node(node, initial, nodes_to_parents)
+            print(f'*** depth: {depth} from {node} ({value}) to {initial} ({maze[initial]})')
+            if keys == keys_to_collect:
+                print('added last key')
+                return depth
+            print(f'added key {value}, recursive step')
+            return depth + min_steps_to_keys(maze, node, keys_to_collect, keys) # restart from here equipped with new key
+
+        for neighbour in get_neighbours(maze, node, keys):
+            if neighbour in explored:
+                continue
+            frontier.append(neighbour)
+            explored.add(neighbour)
+            nodes_to_parents[neighbour] = node
+
+    return None # went through everything and never found goal
+
+def steps_to_node(node_from, node_to, nodes_to_parents):
+    steps = 0
+    parent = nodes_to_parents[node_from]
+    while parent != node_to:
+        steps += 1
+        parent = nodes_to_parents[parent]
+    return steps + 1
+
+def get_neighbours(maze, node, keys):
+    x, y = node
+    neighbours = set()
+    for pos, value in maze.items():
+        if pos not in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
+            continue
+        if pattern_accessible.match(value) or (pattern_door.match(value) and value.lower() in keys):
+            neighbours.add(pos)
+    return neighbours
 
 if __name__ == '__main__':
     maze = read_input()
