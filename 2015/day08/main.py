2e0411eaa3b9fb7e0adf920f6bb3c32fed1d0455
Anders Åhsman <anders.ahsman@gmail.com>
2020-01-12 18:37:03 +0100

    Part 2 done.

--- 2015/day08/main.py
+++ 2015/day08/main.py
@@ -15,5 +15,20 @@ def part1(strings):
         total_value += len(str_value)
     return total_code - total_value
 
+def part2(strings):
+    total_code = 0
+    total_encoded = 0
+    for str_code in strings:
+        total_code += len(str_code)
+        total_encoded += len(escape(str_code))
+    return total_encoded - total_code
+
+def escape(string):
+    return ('"' + string
+        .replace('\\', '\\\\')
+        .replace('"', '\\"')
+        + '"')
+
 strings = read_input()
 print('Part 1:', part1(strings))
+print('Part 2:', part2(strings))
