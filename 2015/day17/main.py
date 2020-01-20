8b5e4e254ab1b45822f658c26e958c47b8901231
Anders Åhsman <anders.ahsman@gmail.com>
2020-01-20 22:12:34 +0100

    Part 2 done.

--- 2015/day17/main.py
+++ 2015/day17/main.py
@@ -2,9 +2,14 @@ import itertools
 import sys
 
 containers = [int(line.strip()) for line in sys.stdin]
-match_cnt = 0
+matches = []
 for bitmask in itertools.product(range(2), repeat=len(containers)):
     comb = [a * b for a, b in zip(containers, bitmask)]
     if sum(comb) == 150:
-        match_cnt += 1
-print('Part 1:', match_cnt)
+        matches.append(comb)
+print('Part 1:', len(matches))
+
+non_zeros = []
+for m in matches:
+    non_zeros.append(len([x for x in m if x != 0]))
+print('Part 2:', sum(1 for x in non_zeros if x == min(non_zeros)))
