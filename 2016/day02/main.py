0b061a9ab00b8a99bb058aea8be2f31e58359ea4
Anders Åhsman <anders.ahsman@gmail.com>
2020-08-28 23:11:01 +0200

    2016 day 2 second part.

--- 2016/day02/main.py
+++ 2016/day02/main.py
@@ -1,25 +1,33 @@
 with open('input.txt', 'r') as f:
     instructions = [line.rstrip() for line in f.readlines()]
 
-keypad = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]]
-x = 1
-y = 1
-code = ''
+keypad_simple = [
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9']]
+keypad_star = [
+    [ '',  '', '1',  '',  ''],
+    [ '', '2', '3', '4',  ''],
+    ['5', '6', '7', '8', '9'],
+    [ '', 'A', 'B', 'C',  ''],
+    [ '',  '', 'D',  '',  '']]
 
-for instruction in instructions:
-    for letter in instruction:
-        if letter == 'U' and y > 0:
-            y -= 1
-        elif letter == 'R' and x < 2:
-            x += 1
-        elif letter == 'D' and y < 2:
-            y += 1
-        elif letter == 'L' and x > 0:
-            x -= 1
+def crack_code(keypad, inst, x, y):
+    code = ''
+    x_max = len(keypad[0]) - 1
+    y_max = len(keypad) - 1
+    for instruction in inst:
+        for letter in instruction:
+            if letter == 'U' and y > 0 and keypad[y - 1][x]:
+                y -= 1
+            elif letter == 'R' and x < x_max and keypad[y][x + 1]:
+                x += 1
+            elif letter == 'D' and y < y_max and keypad[y + 1][x]:
+                y += 1
+            elif letter == 'L' and x > 0 and keypad[y][x - 1]:
+                x -= 1
+        code += keypad[y][x]
+    return code
 
-    code += str(keypad[y][x])
-
-print(code)
+print(f'Part 1: {crack_code(keypad_simple, instructions, 1, 1)}')
+print(f'Part 2: {crack_code(keypad_star, instructions, 0, 2)}')
