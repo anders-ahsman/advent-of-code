dfdb61cb9c9dd0555a3889c7a1ccec828180d7e1
Anders Åhsman <anders.ahsman@gmail.com>
2020-08-29 18:10:37 +0200

    Refactor to make clearer and only read input once.

--- 2016/day03/main.py
+++ 2016/day03/main.py
@@ -1,20 +1,22 @@
 from typing import List, Tuple
 
-def read_input(filename: str) -> List[Tuple[int, int, int]]:
-    candidates: List[Tuple[int, int, int]] = []
+def read_input(filename: str) -> List[List[int]]:
     with open(filename, 'r') as f:
-        numbers = [line.split() for line in f.readlines()]
-        for number in numbers:
-            candidates.append((int(number[0]), int(number[1]), int(number[2])))
+        lines = [line.split() for line in f.readlines()]
+        lines_int = [[int(num) for num in line] for line in lines]
+    return lines_int
+
+def parse_horizontally(lines: List[List[int]]) -> List[Tuple[int, int, int]]:
+    candidates: List[Tuple[int, int, int]] = []
+    for line in lines:
+        candidates.append((line[0], line[1], line[2]))
     return candidates
 
-def read_input_vertically(filename: str) -> List[Tuple[int, int, int]]:
+def parse_vertically(lines: List[List[int]]) -> List[Tuple[int, int, int]]:
     candidates: List[Tuple[int, int, int]] = []
-    with open(filename, 'r') as f:
-        numbers = [line.split() for line in f.readlines()]
-        for y in range(0, len(numbers), 3):
-            for x in range(3):
-                candidates.append((int(numbers[y][x]), int(numbers[y + 1][x]), int(numbers[y + 2][x])))
+    for y in range(0, len(lines), 3):
+        for x in range(3):
+            candidates.append((lines[y][x], lines[y + 1][x], lines[y + 2][x]))
     return candidates
 
 def is_valid_triangle(candidate: Tuple[int, int, int]) -> bool:
@@ -22,8 +24,9 @@ def is_valid_triangle(candidate: Tuple[int, int, int]) -> bool:
     return a + b > c and a + c > b and b + c > a
 
 if __name__ == '__main__':
-    candidates_horizontally = read_input('input.txt')
-    candidates_vertically = read_input_vertically('input.txt')
+    lines = read_input('input.txt')
+    candidates_horizontally = parse_horizontally(lines)
+    candidates_vertically = parse_vertically(lines)
 
     print(f'Part 1: {sum(1 for c in candidates_horizontally if is_valid_triangle(c))}')
     print(f'Part 2: {sum(1 for c in candidates_vertically if is_valid_triangle(c))}')
