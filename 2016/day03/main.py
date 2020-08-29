0e2ae4f9edf0aa8b859a7125550d91633708a13f
Anders Åhsman <anders.ahsman@gmail.com>
2020-08-29 17:52:06 +0200

    2016 day 3 second part.

--- 2016/day03/main.py
+++ 2016/day03/main.py
@@ -8,10 +8,22 @@ def read_input(filename: str) -> List[Tuple[int, int, int]]:
             candidates.append((int(number[0]), int(number[1]), int(number[2])))
     return candidates
 
+def read_input_vertically(filename: str) -> List[Tuple[int, int, int]]:
+    candidates: List[Tuple[int, int, int]] = []
+    with open(filename, 'r') as f:
+        numbers = [line.split() for line in f.readlines()]
+        for y in range(0, len(numbers), 3):
+            for x in range(3):
+                candidates.append((int(numbers[y][x]), int(numbers[y + 1][x]), int(numbers[y + 2][x])))
+    return candidates
+
 def is_valid_triangle(candidate: Tuple[int, int, int]) -> bool:
     a, b, c = candidate
     return a + b > c and a + c > b and b + c > a
 
 if __name__ == '__main__':
-    candidates = read_input('input.txt')
-    print(sum(1 for c in candidates if is_valid_triangle(c)))
+    candidates_horizontally = read_input('input.txt')
+    candidates_vertically = read_input_vertically('input.txt')
+
+    print(f'Part 1: {sum(1 for c in candidates_horizontally if is_valid_triangle(c))}')
+    print(f'Part 2: {sum(1 for c in candidates_vertically if is_valid_triangle(c))}')
