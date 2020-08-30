845d6694f60e9a70fafc144116679526691831a4
Anders Åhsman <anders.ahsman@gmail.com>
2020-08-30 11:06:14 +0200

    2016 day 5 second part.

--- 2016/day05/main.py
+++ 2016/day05/main.py
@@ -1,4 +1,5 @@
 import hashlib
+from typing import List, Optional
 
 def get_password(door_id: str) -> str:
     password = ''
@@ -13,6 +14,25 @@ def get_password(door_id: str) -> str:
         i += 1
     return password
 
+def get_better_password(door_id: str) -> str:
+    password: List[Optional[str]] = [None, None, None, None, None, None, None, None]
+    i = 0
+    while True:
+        arg = str.encode(f'{door_id}{str(i)}')
+        hexdigest = hashlib.md5(arg).hexdigest()
+        if hexdigest[:5] == '00000':
+            try:
+                pos = int(hexdigest[5])
+                if pos < 8 and password[pos] == None:
+                    password[pos] = hexdigest[6]
+                    if None not in password:
+                        break
+            except ValueError:
+                pass
+        i += 1
+    return ''.join([str(ch) for ch in password])
+
 if __name__ == '__main__':
     door_id = 'abbhdwsy'
-    print(get_password(door_id))
+    print(f'Part 1: {get_password(door_id)}')
+    print(f'Part 2: {get_better_password(door_id)}')
