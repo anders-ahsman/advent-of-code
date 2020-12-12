f18e99d3423cfafc5ef11db91459edb7463e80a4
Anders Åhsman <anders.ahsman@gmail.com>
2020-12-12 22:27:32 +0100

    2020 day 12 complete.

--- 2020/day12/main.py
+++ 2020/day12/main.py
@@ -12,7 +12,6 @@ def part1(instructions):
     for instruction in instructions:
         action = instruction[:1]
         value = int(instruction[1:])
-
         if action == 'N':
             y -= value
         elif action == 'S':
@@ -36,10 +35,46 @@ def part1(instructions):
                 x -= value
         else:
             raise ValueError(f'Unknown instruction "{action}"')
+    return abs(x) + abs(y)
 
+
+def part2(instructions):
+    x, y = 0, 0
+    wx, wy = 10, 1
+    for instruction in instructions:
+        action = instruction[:1]
+        value = int(instruction[1:])
+        if action == 'N':
+            wy += value
+        elif action == 'S':
+            wy -= value
+        elif action == 'W':
+            wx -= value
+        elif action == 'E':
+            wx += value
+        elif action == 'L':
+            if value == 90:
+                wx, wy = -wy, wx
+            elif value == 180:
+                wx, wy = -wx, -wy
+            elif value == 270:
+                wx, wy = wy, -wx
+        elif action == 'R':
+            if value == 90:
+                wx, wy = wy, -wx
+            elif value == 180:
+                wx, wy = -wx, -wy
+            elif value == 270:
+                wx, wy = -wy, wx
+        elif action == 'F':
+            x += wx * value
+            y += wy * value
+        else:
+            raise ValueError(f'Unknown instruction "{action}"')
     return abs(x) + abs(y)
 
 
 if __name__ == '__main__':
     instructions = read_instructions()
     print(f'Part 1: {part1(instructions)}')
+    print(f'Part 2: {part2(instructions)}')
