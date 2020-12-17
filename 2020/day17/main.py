37ee2f9d7904e5fe7afc8d07f39252f2186484f5
Anders Åhsman <anders.ahsman@gmail.com>
2020-12-17 22:18:29 +0100

    2020 day 17 complete, lots of duplicated code.

--- 2020/day17/main.py
+++ 2020/day17/main.py
@@ -1,11 +1,15 @@
 import sys
 
 
-def read_initial_state():
+def read_lines():
+    return [line.rstrip() for line in sys.stdin]
+
+
+def read_initial_state(lines):
     state = {}
     z = 0
-    for y, line in enumerate(sys.stdin):
-        for x, ch in enumerate(line.rstrip()):
+    for y, line in enumerate(lines):
+        for x, ch in enumerate(line):
             state[(x, y, z)] = ch
     return state
 
@@ -40,6 +44,53 @@ def neighbours(state, pos):
                     count += 1
     return count
 
+
+def read_initial_state2(lines):
+    state = {}
+    w = 0
+    z = 0
+    for y, line in enumerate(lines):
+        for x, ch in enumerate(line):
+            state[(x, y, z, w)] = ch
+    return state
+
+
+def part2(state):
+    for _ in range(6):
+        newstate = dict(state)
+        for pos in state:
+            x, y, z, w = pos
+            for dw in (-1, 0, 1):
+                for dz in (-1, 0, 1):
+                    for dy in (-1, 0, 1):
+                        for dx in (-1, 0, 1):
+                            newpos = (x + dx, y + dy, z + dz, w + dw)
+                            if newpos in state and state[newpos] == '#':
+                                newstate[newpos] = '#' if 2 <= neighbours2(state, newpos) <= 3 else '.'
+                            elif newpos in state and state[newpos] == '.' or newpos not in state:
+                                newstate[newpos] = '#' if neighbours2(state, newpos) == 3 else '.'
+        state = newstate
+    return list(state.values()).count('#')
+
+
+def neighbours2(state, pos):
+    count = 0
+    x, y, z, w = pos
+    for dw in (-1, 0, 1):
+        for dz in (-1, 0, 1):
+            for dy in (-1, 0, 1):
+                for dx in (-1, 0, 1):
+                    if dz == dy == dx == dw == 0:
+                        continue
+                    npos = (x + dx, y + dy, z + dz, w + dw)
+                    if npos in state and state[npos] == '#':
+                        count += 1
+    return count
+
+
 if __name__ == '__main__':
-    initial_state = read_initial_state()
+    lines = read_lines()
+    initial_state = read_initial_state(lines)
+    initial_state2 = read_initial_state2(lines)
     print(f'Part 1: {part1(initial_state)}')
+    print(f'Part 2: {part2(initial_state2)}')
