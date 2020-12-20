e425e8d37fb4f0fe668feb4cf47e0e17e549b709
Anders Åhsman <anders.ahsman@gmail.com>
2020-12-20 10:24:24 +0100

    Both parts complete.

--- 2020/day18/main.py
+++ 2020/day18/main.py
@@ -10,22 +10,33 @@ def part1(expressions):
     return sum(solve(expr) for expr in expressions)
 
 
-def solve(expr):
+def part2(expressions):
+    return sum(solve(expr, True) for expr in expressions)
+
+
+def solve(expr, is_part2=False):
     expr = expr.replace(' ', '')
 
     while '(' in expr:
         start = expr.rindex('(') + 1
         end = start + expr[start:].index(')')
-        res = solve(expr[start:end]) # sub-expression contains no parenthesis
+        res = solve(expr[start:end], is_part2) # sub-expression contains no parenthesis
         expr = f'{expr[:start - 1]}{res}{expr[end + 1:]}' # replace sub-expression with result
 
-    while '+' in expr or '*' in expr:
-        # since start of string is matched order will be left to right
-        expr = re.sub(r'^(\d+)\+(\d+)', lambda m: str(int(m[1]) + int(m[2])), expr)
-        expr = re.sub(r'^(\d+)\*(\d+)', lambda m: str(int(m[1]) * int(m[2])), expr)
+    if is_part2:
+        while '+' in expr:
+            expr = re.sub(r'(\d+)\+(\d+)', lambda m: str(int(m[1]) + int(m[2])), expr)
+        while '*' in expr :
+            expr = re.sub(r'(\d+)\*(\d+)', lambda m: str(int(m[1]) * int(m[2])), expr)
+    else:
+        while '+' in expr or '*' in expr:
+            # since start of string is matched order will be left to right
+            expr = re.sub(r'^(\d+)\+(\d+)', lambda m: str(int(m[1]) + int(m[2])), expr)
+            expr = re.sub(r'^(\d+)\*(\d+)', lambda m: str(int(m[1]) * int(m[2])), expr)
 
     return int(expr)
 
 if __name__ == '__main__':
     expressions = read_expressions()
     print(f'Part 1: {part1(expressions)}')
+    print(f'Part 2: {part2(expressions)}')
