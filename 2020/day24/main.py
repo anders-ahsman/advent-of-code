0d83cf21ae672287bf8bb111bb5e1609928054a5
Anders Åhsman <anders.ahsman@gmail.com>
2020-12-25 20:53:29 +0100

    2020 day 24 complete.

--- 2020/day24/main.py
+++ 2020/day24/main.py
@@ -7,7 +7,7 @@ def read_lines():
 
 
 def part1(lines):
-    tiles = {}
+    black_tiles = set()
     for line in lines:
         x, y, z = 0, 0, 0
         while len(line) > 0:
@@ -37,10 +37,45 @@ def part1(lines):
                 y -= 1
             else:
                 raise ValueError(f'No match for line: {line}')
-        tiles[(x, y, z)] = True if (x, y, z) not in tiles else not tiles[(x, y, z)]
-    return sum(1 for val in tiles.values() if val)
+        pos = (x, y, z)
+        if pos in black_tiles:
+            black_tiles.remove(pos)
+        else:
+            black_tiles.add(pos)
+    return black_tiles
+
+
+def part2(black_tiles):
+    for _ in range(100):
+        new_black = black_tiles.copy()
+        for tile in black_tiles:
+            # check black tile and its neighbours
+            positions = [(0, 0, 0), (0, 1, -1), (-1, 0, 1), (1, 0, -1), (0, -1, 1), (-1, 1, 0), (1, -1, 0)]
+            for (dx, dy, dz) in positions:
+                (x, y, z) = tile
+                pos = (x + dx, y + dy, z + dz)
+                count = black_neighbour_count(pos, black_tiles)
+                if pos in black_tiles and (count == 0 or count > 2) and pos in new_black:
+                    new_black.remove(pos)
+                elif pos not in black_tiles and count == 2:
+                    new_black.add(pos)
+        black_tiles = new_black
+    return black_tiles
+
+
+def black_neighbour_count(tile, black_tiles):
+    count = 0
+    neighbours = [(0, 1, -1), (-1, 0, 1), (1, 0, -1), (0, -1, 1), (-1, 1, 0), (1, -1, 0)]
+    for (dx, dy, dz) in neighbours:
+        (x, y, z) = tile
+        pos = (x + dx, y + dy, z + dz)
+        if pos in black_tiles:
+            count += 1
+    return count
 
 
 if __name__ == '__main__':
     lines = read_lines()
-    print(f'Part 1: {part1(lines)}')
+    black_tiles = part1(lines)
+    print(f'Part 1: {len(black_tiles)}')
+    print(f'Part 2: {len(part2(black_tiles))}')
