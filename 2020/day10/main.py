9a03236a0b641209689adacf9715a9d8abb226b7
Anders Åhsman <anders.ahsman@gmail.com>
2020-12-31 22:05:59 +0100

    Cleaner solution using sum and zip.

--- 2020/day10/main.py
+++ 2020/day10/main.py
@@ -6,23 +6,13 @@ def read_jolts():
 
 
 def part1(jolts):
-    diff1_count = 0
-    diff3_count = 0
-    for i in range(1, len(jolts)):
-        jolt = jolts[i]
-        last_jolt = jolts[i - 1]
-        if jolt == last_jolt + 1:
-            diff1_count +=1
-        elif jolt == last_jolt + 3:
-            diff3_count += 1
-        else:
-            raise ValueError(jolt, last_jolt)
+    jolts += [0, max(jolts) + 3]
+    jolts = sorted(jolts)
+    diff1_count = sum(1 for a, b in zip(jolts, jolts[1:]) if b - a == 1)
+    diff3_count = sum(1 for a, b in zip(jolts, jolts[1:]) if b - a == 3)
     return diff1_count * diff3_count
 
 
 if __name__ == '__main__':
     jolts = read_jolts()
-    jolts += [0, max(jolts) + 3]
-    jolts = sorted(jolts)
-
     print(f'Part 1: {part1(jolts)}')
