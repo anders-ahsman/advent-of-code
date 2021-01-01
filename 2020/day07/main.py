72f42a7904f051c83e2d8c24b115ede1df72e319
Anders Åhsman <anders.ahsman@gmail.com>
2021-01-01 19:14:37 +0100

    Prepare for second part using dict to include bag count, better variable names.

--- 2020/day07/main.py
+++ 2020/day07/main.py
@@ -6,29 +6,30 @@ def read_lines():
 
 
 def create_graph(lines):
-    graph = {}
+    bag_to_contents = {}
     for line in lines:
-        bag_type, contain = line.rstrip().split(' bags contain ')
-        if 'no other' in contain:
-            contain = []
-        else:
-            contain = [part for part in contain.split(', ')]
-        graph[bag_type] = contain
-    return graph
+        bag, inside = line.rstrip().split(' bags contain ')
+        contents = {}
+        if 'no other' not in inside:
+            for part in inside.split(', '):
+                count, color = int(part[0:1]), part[2:]
+                contents[color] = count
+        bag_to_contents[bag] = contents
+    return bag_to_contents
 
 
-def part1(graph, target, count=0, seen=set()):
+def part1(bag_to_contents, target, count=0, seen=set()):
     if target in seen:
         return count - 1
     seen.add(target)
-    for bag, contain in graph.items():
-        if any(target in part for part in contain):
-            count = part1(graph, bag, count + 1)
+    for bag in bag_to_contents:
+        if any(target in part for part in bag_to_contents[bag]):
+            count = part1(bag_to_contents, bag, count + 1)
     return count
 
 
 if __name__ == '__main__':
     lines = read_lines()
-    graph = create_graph(lines)
+    bag_to_contents = create_graph(lines)
     target = 'shiny gold'
-    print(f'Part 1: {part1(graph, target)}')
+    print(f'Part 1: {part1(bag_to_contents, target)}')
