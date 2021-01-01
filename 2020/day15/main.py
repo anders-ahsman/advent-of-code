82710f22b78ba8d6e8a2c91357d70e60a27a14b7
Anders Åhsman <anders.ahsman@gmail.com>
2021-01-02 00:00:59 +0100

    2020 day 15 complete.

--- 2020/day15/main.py
+++ 2020/day15/main.py
@@ -1,19 +1,24 @@
-def part1(numbers):
-    def rindex(l, val):
-        return len(l) - l[::-1].index(val) - 1
+from collections import defaultdict
 
-    for _ in range(len(numbers), 2020):
-        last_number = numbers[-1]
-        if numbers.count(last_number) > 1:
-            idx_r = rindex(numbers, last_number)
-            idx_l = rindex(numbers[:idx_r], last_number)
-            next_number = idx_r - idx_l
+
+def solve(numbers, turns):
+    num_to_turns_spoken = defaultdict(list)
+
+    last_num = None
+    for i, num in enumerate(numbers):
+        num_to_turns_spoken[num].append(i)
+        last_num = num
+
+    for turn in range(len(numbers), turns):
+        if len(num_to_turns_spoken[last_num]) > 1:
+            last_num = num_to_turns_spoken[last_num][-1] - num_to_turns_spoken[last_num][-2]
         else:
-            next_number = 0
-        numbers.append(next_number)
-    return numbers[-1]
+            last_num = 0
+        num_to_turns_spoken[last_num].append(turn)
+    return last_num
 
 
 if __name__ == '__main__':
     numbers = [1, 0, 16, 5, 17, 4]
-    print(f'Part 1: {part1(numbers)}')
+    print(f'Part 1: {solve(numbers, 2020)}')
+    print(f'Part 2: {solve(numbers, 30_000_000)}')
