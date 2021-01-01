95f4827456d65e6131c2bfa61adb1e19ae1f73ef
Anders Åhsman <anders.ahsman@gmail.com>
2021-01-01 18:47:09 +0100

    2020 day 10 complete.

--- 2020/day10/main.py
+++ 2020/day10/main.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import sys
 
 
@@ -6,13 +7,31 @@ def read_jolts():
 
 
 def part1(jolts):
-    jolts += [0, max(jolts) + 3]
-    jolts = sorted(jolts)
+    jolts = sorted(jolts + [0, max(jolts) + 3])
     diff1_count = sum(1 for a, b in zip(jolts, jolts[1:]) if b - a == 1)
     diff3_count = sum(1 for a, b in zip(jolts, jolts[1:]) if b - a == 3)
     return diff1_count * diff3_count
 
 
+def part2(jolts):
+    jolts = sorted(jolts + [0, max(jolts) + 3])
+    connected_count = defaultdict(int)
+    length = 1
+    for jolt, last_jolt in zip(jolts[1:], jolts):
+        if jolt - last_jolt == 1:
+            length += 1
+        else:
+            if length > 2:
+                connected_count[length] += 1
+            length = 1
+    res = 1
+    factors = {3: 2, 4: 4, 5: 7}
+    for length, count in connected_count.items():
+        res *= factors[length] ** count
+    return res
+
+
 if __name__ == '__main__':
     jolts = read_jolts()
     print(f'Part 1: {part1(jolts)}')
+    print(f'Part 2: {part2(jolts)}')
