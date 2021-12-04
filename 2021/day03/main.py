4c085dd9b0227190d10d85b02f79f172f210c734
Anders Åhsman <anders.ahsman@gmail.com>
2021-12-04 20:08:36 +0100

    Refactor shared code.

--- 2021/day03/main.py
+++ 2021/day03/main.py
@@ -13,8 +13,8 @@ def part1(numbers: List[str]) -> int:
     epsilon = ''
 
     for i in range(len(numbers[0])):
-        ones = sum(1 for number in numbers if number[i] == '1')
-        zeros = sum(1 for number in numbers if number[i] == '0')
+        ones = ones_count(numbers, i)
+        zeros = zeros_count(numbers, i)
         gamma += '1' if ones > zeros else '0'
         epsilon += '0' if ones > zeros else '1'
 
@@ -24,12 +24,8 @@ def part1(numbers: List[str]) -> int:
 def part2(numbers: List[str]) -> int:
     def get_oxygen_rating(numbers: List[str]) -> int:
         for i in range(len(numbers[0])):
-            ones = sum(1 for number in numbers if number[i] == '1')
-            zeros = sum(1 for number in numbers if number[i] == '0')
-            if ones >= zeros:
-                numbers = [n for n in numbers if n[i] == '1']
-            else:
-                numbers = [n for n in numbers if n[i] == '0']
+            more_ones = ones_count(numbers, i) >= zeros_count(numbers, i)
+            numbers = numbers_with_one_at_idx(numbers, i) if more_ones else numbers_with_zero_at_idx(numbers, i)
 
             if len(numbers) == 1:
                 return int(numbers[0], 2)
@@ -37,12 +33,8 @@ def part2(numbers: List[str]) -> int:
 
     def get_scrubber_rating(numbers: List[str]) -> int:
         for i in range(len(numbers[0])):
-            ones = sum(1 for number in numbers if number[i] == '1')
-            zeros = sum(1 for number in numbers if number[i] == '0')
-            if ones < zeros:
-                numbers = [n for n in numbers if n[i] == '1']
-            else:
-                numbers = [n for n in numbers if n[i] == '0']
+            more_zeros = ones_count(numbers, i) < zeros_count(numbers, i)
+            numbers = numbers_with_one_at_idx(numbers, i) if more_zeros else numbers_with_zero_at_idx(numbers, i)
 
             if len(numbers) == 1:
                 return int(numbers[0], 2)
@@ -51,6 +43,22 @@ def part2(numbers: List[str]) -> int:
     oxygen_rating = get_oxygen_rating(numbers)
     scrubber_rating = get_scrubber_rating(numbers)
     return oxygen_rating * scrubber_rating
+
+
+def ones_count(numbers: List[str], idx: int) -> int:
+    return sum(1 for number in numbers if number[idx] == '1')
+
+
+def zeros_count(numbers: List[str], idx: int) -> int:
+    return sum(1 for number in numbers if number[idx] == '0')
+
+
+def numbers_with_one_at_idx(numbers: List[str], idx: int) -> List[str]:
+    return [n for n in numbers if n[idx] == '1']
+
+
+def numbers_with_zero_at_idx(numbers: List[str], idx: int) -> List[str]:
+    return [n for n in numbers if n[idx] == '0']
 
 
 if __name__ == '__main__':
