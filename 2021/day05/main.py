d3966eeb1a39cb0598af6f69825fd6a7c02e1b67
Anders Åhsman <anders.ahsman@gmail.com>
2021-12-05 11:45:18 +0100

    2021 day 5 first part.

---
+++ 2021/day05/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import defaultdict
+from typing import List
+
+
+def read_input() -> List[str]:
+    return [line.strip() for line in sys.stdin]
+
+
+def part1(lines: List[str]) -> int:
+    board = defaultdict(int)
+    for line in lines:
+        pos_from, pos_to = line.split(' -> ')
+        from_x, from_y = [int(pos) for pos in pos_from.split(',')]
+        to_x, to_y = [int(pos) for pos in pos_to.split(',')]
+        if from_x == to_x:
+            for y in range(from_y, to_y + 1) if from_y <= to_y else range(to_y, from_y + 1):
+                board[(from_x, y)] += 1
+        if from_y == to_y:
+            for x in range(from_x, to_x + 1) if from_x <= to_x else range(to_x, from_x + 1):
+                board[(x, from_y)] += 1
+    return sum(1 for overlap in board.values() if overlap >= 2)
+
+
+if __name__ == '__main__':
+    lines = read_input()
+    print(f'Part 1: {part1(lines)}')
