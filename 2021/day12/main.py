bb1a734abb4dca8adaff1cdfb852c312c04ee957
Anders Åhsman <anders.ahsman@gmail.com>
2022-01-04 18:40:44 +0100

    2021 day 12 second part.

--- 2021/day12/main.py
+++ 2021/day12/main.py
@@ -38,7 +38,38 @@ def part1(connections: DefaultDict[str, Set[str]]) -> int:
     return path_count
 
 
+def part2(connections: DefaultDict[str, Set[str]]) -> int:
+    path_count = 0
+
+    def visit_node(node: str, explored_count: DefaultDict[str, int]) -> None:
+        explored_count[node] += 1
+
+        if node == 'end':
+            nonlocal path_count
+            path_count += 1
+            return
+
+        for neighbour in connections[node]:
+            if neighbour.islower():
+                if neighbour == 'start':
+                    continue
+
+                if explored_count[neighbour] >= 2:
+                    continue
+
+                any_lower_visited_twice = any(v == 2 for k, v in explored_count.items() if k.islower())
+                if neighbour != 'end' and explored_count[neighbour] == 1 and any_lower_visited_twice:
+                    continue
+
+            visit_node(neighbour, explored_count.copy())
+
+    visit_node('start', defaultdict(int))
+
+    return path_count
+
+
 if __name__ == '__main__':
     lines = read_input()
     connections = parse_connections(lines)
     print(f'Part 1: {part1(connections)}')
+    print(f'Part 2: {part2(connections)}')
