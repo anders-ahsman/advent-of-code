beccd2312d6d48cab6113dc6e58b54f9d876432d
Anders Åhsman <anders.ahsman@gmail.com>
2024-12-11 16:34:37 +0100

    2024 day 2 second part.

--- 2024/day02/main.py
+++ 2024/day02/main.py
@@ -7,25 +7,36 @@ def read_input() -> list[list[int]]:
     return [list(map(int, line.strip().split())) for line in sys.stdin]
 
 
-def solve_part1(reports: list[list[int]]) -> int:
+def solve(reports: list[list[int]], part2: bool) -> int:
     safe = 0
 
     for report in reports:
-        increasing = 0
-        decreasing = 0
-        count = len(report) - 1
-        for i in range(count):
-            if 1 <= report[i + 1] - report[i] <= 3:
-                increasing += 1
-            elif 1 <= report[i] - report[i + 1] <= 3:
-                decreasing += 1
-
-        if increasing == count or decreasing == count:
+        if is_safe(report):
             safe += 1
+        elif part2:
+            for i in range(len(report)):
+                modified = report[:i] + report[i + 1 :]
+                if is_safe(modified):
+                    safe += 1
+                    break
 
     return safe
 
 
+def is_safe(report: list[int]) -> bool:
+    increasing = 0
+    decreasing = 0
+    count = len(report) - 1
+    for i in range(count):
+        if 1 <= report[i + 1] - report[i] <= 3:
+            increasing += 1
+        elif 1 <= report[i] - report[i + 1] <= 3:
+            decreasing += 1
+
+    return (increasing == count) or (decreasing == count)
+
+
 if __name__ == '__main__':
     reports = read_input()
-    print(solve_part1(reports))
+    print(solve(reports, False))
+    print(solve(reports, True))
