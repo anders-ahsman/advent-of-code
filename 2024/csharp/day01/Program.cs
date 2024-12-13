0e6acbacee9a6d0361a745f6b0d4e5b36fffe80c
Anders Åhsman <anders.ahsman@gmail.com>
2024-12-13 22:28:13 +0100

    2024 day 1 C#.

---
+++ 2024/csharp/day01/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace day01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var left = new List<int>();
+        var right = new List<int>();
+        string[] lines = File.ReadAllLines("input.txt");
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("  ");
+            left.Add(int.Parse(parts[0]));
+            right.Add(int.Parse(parts[1]));
+        }
+        left.Sort();
+        right.Sort();
+
+        int totalDifference = left.Zip(right, (l, r) => Math.Abs(l - r)).Sum();
+
+        var rightCounts = right.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+        var similarityScore = left.Sum(l => l * rightCounts.GetValueOrDefault(l, 0));
+
+        Console.WriteLine("Part 1: " + totalDifference);
+        Console.WriteLine("Part 2: " + similarityScore);
+    }
+}
