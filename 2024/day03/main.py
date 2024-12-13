cd2801c2e1ac08fd795b6b09782c34fe4fb51c95
Anders Åhsman <anders.ahsman@gmail.com>
2024-12-13 21:47:21 +0100

    Second part.

--- 2024/day03/main.py
+++ 2024/day03/main.py
@@ -3,15 +3,18 @@
 import re
 import sys
 
+pattern_mul = re.compile(r'mul\((\d+),(\d+)\)')
+pattern_do = re.compile(r'do\(\)')
+pattern_dont = re.compile(r'don\'t\(\)')
+
 
 def read_input() -> str:
     lines = [line.strip() for line in sys.stdin]
     return ''.join(lines)
 
 
-def solve(memory: str, part2: bool) -> int:
-    pattern = r'mul\((\d+),(\d+)\)'
-    matches = re.findall(pattern, memory)
+def part1(memory: str) -> int:
+    matches = re.findall(pattern_mul, memory)
     sum = 0
     for match in matches:
         x, y = map(int, match)
@@ -20,7 +23,39 @@ def solve(memory: str, part2: bool) -> int:
     return sum
 
 
+def part2(memory: str) -> int:
+    sum = 0
+    pos = 0
+    is_on = True
+
+    while pos < len(memory):
+        memory_chunk = memory[pos:]
+        match_mul = re.search(pattern_mul, memory_chunk)
+        match_do = re.search(pattern_do, memory_chunk)
+        match_dont = re.search(pattern_dont, memory_chunk)
+
+        next_mul = match_mul.start() if match_mul else len(memory)
+        next_do = match_do.start() if match_do else len(memory)
+        next_dont = match_dont.start() if match_dont else len(memory)
+
+        if match_mul and next_mul < next_do and next_mul < next_dont:
+            if is_on:
+                x, y = map(int, match_mul.groups())
+                sum += x * y
+            pos += match_mul.end()
+        elif match_do and next_do < next_mul and next_do < next_dont:
+            is_on = True
+            pos += match_do.end()
+        elif match_dont and next_dont < next_mul and next_dont < next_do:
+            is_on = False
+            pos += match_dont.end()
+        else:
+            break
+
+    return sum
+
+
 if __name__ == '__main__':
     memory = read_input()
-    print(solve(memory, False))
-    # print(solve(memory, True))
+    print(part1(memory))
+    print(part2(memory))
