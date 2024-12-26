54b432010a4c62bb4a162a94690ce5ae06d46f93
Anders Åhsman <anders.ahsman@gmail.com>
2024-12-26 08:54:11 +0100

    Second part.

--- 2024/day06/main.py
+++ 2024/day06/main.py
@@ -18,8 +18,8 @@ class Direction:
 
 @dataclass(frozen=True)
 class Board:
-    max_x: int
-    max_y: int
+    width: int
+    height: int
 
 
 @dataclass(frozen=True)
@@ -29,8 +29,8 @@ class Guard:
 
 
 def read_input() -> tuple[set[Position], Guard, Board]:
-    max_x = 0
-    max_y = 0
+    width = 0
+    height = 0
     obstructions: set[Position] = set()
     guard: Guard = Guard(Position(0, 0), Direction(0, 0))
 
@@ -46,10 +46,10 @@ def read_input() -> tuple[set[Position], Guard, Board]:
             else:
                 raise ValueError(f'Invalid character: {char}')
 
-            max_x = max(max_x, x)
-            max_y = max(max_y, y)
+            width = max(width, x)
+            height = max(height, y)
 
-    board = Board(max_x, max_y)
+    board = Board(width, height)
 
     return obstructions, guard, board
 
@@ -57,15 +57,23 @@ def read_input() -> tuple[set[Position], Guard, Board]:
 def part1(obstructions: set[Position], guard: Guard, board: Board) -> int:
     seen: set[Position] = set()
 
-    while guard.pos.x >= 0 and guard.pos.x <= board.max_x and guard.pos.y >= 0 and guard.pos.y <= board.max_y:
+    while is_within_bounds(guard, board):
         seen.add(guard.pos)
-        new_pos = Position(guard.pos.x + guard.dir.x, guard.pos.y + guard.dir.y)
+        new_pos = get_next_guard_position(guard)
         if new_pos in obstructions:
             guard = Guard(guard.pos, turn_right(guard.dir))
         else:
             guard = Guard(new_pos, guard.dir)
 
     return len(seen)
+
+
+def is_within_bounds(guard: Guard, board: Board) -> bool:
+    return guard.pos.x >= 0 and guard.pos.x <= board.width and guard.pos.y >= 0 and guard.pos.y <= board.height
+
+
+def get_next_guard_position(guard: Guard) -> Position:
+    return Position(guard.pos.x + guard.dir.x, guard.pos.y + guard.dir.y)
 
 
 def turn_right(dir: Direction) -> Direction:
@@ -81,6 +89,37 @@ def turn_right(dir: Direction) -> Direction:
         raise ValueError(f'Invalid direction: {dir}')
 
 
+def part2(obstructions: set[Position], guard: Guard, board: Board) -> int:
+    count = 0
+    obstructions_original: set[Position] = obstructions.copy()
+
+    for y in range(board.height + 1):
+        for x in range(board.width + 1):
+            new_obstruction = Position(x, y)
+            if new_obstruction not in obstructions and new_obstruction != guard.pos:
+                obstructions = obstructions_original.copy()
+                obstructions.add(new_obstruction)
+                if is_loop(obstructions, guard, board):
+                    count += 1
+
+    return count
+
+
+def is_loop(obstructions: set[Position], guard: Guard, board: Board) -> bool:
+    trail: set[Guard] = set()
+
+    while is_within_bounds(guard, board) and guard not in trail:
+        trail.add(guard)
+        new_pos = get_next_guard_position(guard)
+        if new_pos in obstructions:
+            guard = Guard(guard.pos, turn_right(guard.dir))
+        else:
+            guard = Guard(new_pos, guard.dir)
+
+    return guard in trail
+
+
 if __name__ == '__main__':
     obstructions, guard, board = read_input()
     print(f'Part 1: {part1(obstructions, guard, board)}')
+    print(f'Part 2: {part2(obstructions, guard, board)}')
