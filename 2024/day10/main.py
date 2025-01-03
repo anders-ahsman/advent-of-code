969284a176b2d2a0b1097dd5ffbc1bf25a0e61c5
Anders Åhsman <anders.ahsman@gmail.com>
2025-01-03 08:57:19 +0100

    Second part.

--- 2024/day10/main.py
+++ 2024/day10/main.py
@@ -16,13 +16,6 @@ def read_input() -> list[list[int]]:
     return [list(map(int, line.strip())) for line in sys.stdin]
 
 
-def part1(map_raw: list[list[int]]) -> int:
-    map_topo = parse_raw_map(map_raw)
-    trail_heads = [pos for pos in map_topo if pos.height == 0]
-
-    return find_path_count(map_topo, trail_heads)
-
-
 def parse_raw_map(map_raw: list[list[int]]) -> list[Position]:
     map_topo: list[Position] = []
 
@@ -32,6 +25,18 @@ def parse_raw_map(map_raw: list[list[int]]) -> list[Position]:
             map_topo.append(Position(x, y, h))
 
     return map_topo
+
+
+def part1(map_topo: list[Position]) -> int:
+    trail_heads = [pos for pos in map_topo if pos.height == 0]
+
+    return find_path_count(map_topo, trail_heads)
+
+
+def part2(map_topo: list[Position]) -> int:
+    trail_heads = [pos for pos in map_topo if pos.height == 0]
+
+    return find_possible_path_count(map_topo, trail_heads)
 
 
 def find_path_count(map_topo: list[Position], trail_heads: list[Position]) -> int:
@@ -47,6 +52,7 @@ def find_path_count(map_topo: list[Position], trail_heads: list[Position]) -> in
 
             if pos.height == 9:
                 path_count += 1
+                continue
 
             for dx, dy in directions:
                 new_pos = Position(pos.x + dx, pos.y + dy, pos.height + 1)
@@ -57,6 +63,32 @@ def find_path_count(map_topo: list[Position], trail_heads: list[Position]) -> in
     return path_count
 
 
+def find_possible_path_count(map_topo: list[Position], trail_heads: list[Position]) -> int:
+    path_count = 0
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+    for trail_head in trail_heads:
+        queue: deque[list[Position]] = deque([[trail_head]])
+
+        while queue:
+            path = queue.popleft()
+            pos = path[-1]
+
+            if pos.height == 9:
+                path_count += 1
+                continue
+
+            for dx, dy in directions:
+                new_pos = Position(pos.x + dx, pos.y + dy, pos.height + 1)
+                if new_pos in map_topo:
+                    new_path = path + [new_pos]
+                    queue.append(new_path)
+
+    return path_count
+
+
 if __name__ == '__main__':
     map_raw = read_input()
-    print(f'Part 1: {part1(map_raw)}')
+    map_topo = parse_raw_map(map_raw)
+    print(f'Part 1: {part1(map_topo)}')
+    print(f'Part 2: {part2(map_topo)}')
