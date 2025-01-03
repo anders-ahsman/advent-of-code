adeb0c250635a43740195fdaaca94255f09a31a6
Anders Åhsman <anders.ahsman@gmail.com>
2025-01-03 09:28:07 +0100

    Refactor: simplify code.

--- 2024/day10/main.py
+++ 2024/day10/main.py
@@ -30,16 +30,16 @@ def parse_raw_map(map_raw: list[list[int]]) -> list[Position]:
 def part1(map_topo: list[Position]) -> int:
     trail_heads = [pos for pos in map_topo if pos.height == 0]
 
-    return find_path_count(map_topo, trail_heads)
+    return find_path_count(map_topo, trail_heads, False)
 
 
 def part2(map_topo: list[Position]) -> int:
     trail_heads = [pos for pos in map_topo if pos.height == 0]
 
-    return find_possible_path_count(map_topo, trail_heads)
+    return find_path_count(map_topo, trail_heads, True)
 
 
-def find_path_count(map_topo: list[Position], trail_heads: list[Position]) -> int:
+def find_path_count(map_topo: list[Position], trail_heads: list[Position], all_possible: bool) -> int:
     path_count = 0
     directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
@@ -57,32 +57,9 @@ def find_path_count(map_topo: list[Position], trail_heads: list[Position]) -> in
             for dx, dy in directions:
                 new_pos = Position(pos.x + dx, pos.y + dy, pos.height + 1)
                 if new_pos not in visited and new_pos in map_topo:
-                    visited.add(new_pos)
                     queue.append(new_pos)
-
-    return path_count
-
-
-def find_possible_path_count(map_topo: list[Position], trail_heads: list[Position]) -> int:
-    path_count = 0
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-    for trail_head in trail_heads:
-        queue: deque[list[Position]] = deque([[trail_head]])
-
-        while queue:
-            path = queue.popleft()
-            pos = path[-1]
-
-            if pos.height == 9:
-                path_count += 1
-                continue
-
-            for dx, dy in directions:
-                new_pos = Position(pos.x + dx, pos.y + dy, pos.height + 1)
-                if new_pos in map_topo:
-                    new_path = path + [new_pos]
-                    queue.append(new_path)
+                    if not all_possible:
+                        visited.add(new_pos)
 
     return path_count
 
